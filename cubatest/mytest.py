65c36495e104f99c9fe9ea54e9925e504937a2a8
aravindhshankar <aravindhswaminathan@gmail.com>
2024-11-07 20:17:53 +0100

    this cuhre thing sort of works for two sharp peaks

--- cubatest/mytest.py
+++ cubatest/mytest.py
@@ -6,67 +6,90 @@ import pycuba
 #pycuba.demo()
 import numpy as np
 
+def print_header(name):
+	print('-------------------- %s test -------------------' % name)
+def print_results(name, results):
+	keys = ['nregions', 'neval', 'fail']
+	text = ["%s %d" % (k, results[k]) for k in keys if k in results]
+	print("%s RESULT:\t" % name.upper() + "\t".join(text))
+	for comp in results['results']:
+		print("%s RESULT:\t" % name.upper() + \
+			"%(integral).8f +- %(error).8f\tp = %(prob).3f\n" % comp)
+
+def cubify(f,a,b):
+	'''
+	f(x) is integrated from a to b
+	g(y) is integrated from 0 to 1 to give the same result
+	'''
+
+	return lambda x: (b-a) * f(a + (b-a)*y) 
+ 
+
+
 if __name__ == '__main__':
 	import math
 
 	def Integrand(ndim, xx, ncomp, ff, userdata):
 		x,y,z = [xx[i] for i in range(ndim.contents.value)]
-		result = math.sin(x)*math.cos(y)*math.exp(z)
+		result = np.exp(-x**2)
+		ff[0] = result
+		return 0
+	def sharpIntegrand(ndim, xx, ncomp, ff, userdata):
+		x,_ = [xx[i] for i in range(ndim.contents.value)]
+		a = 1e-5
+		pref = 1 / (a * np.pi) 
+		result = pref * 1 / ( 1 + ((x-0.1312)**2 / a**2))
+		ff[0] = result
+		return 0
+	def twopeakIntegrand(ndim, xx, ncomp, ff, userdata):
+		x,_ = [xx[i] for i in range(ndim.contents.value)]
+		a = 1e-5
+		pref = 1 / (a * np.pi) 
+		result = pref * 1 / ( 1 + ((x-0.1312)**2 / a**2)) + pref * 1 / ( 1 + ((x-0.43217)**2 / a**2))
+ 
 		ff[0] = result
 		return 0
 
-
-	NDIM = 3
+	NDIM = 2
 	NCOMP = 1
 
 	NNEW = 1000
 	NMIN = 2
-	FLATNESS = 50.
+	# FLATNESS = 50.
+	FLATNESS = 0.1
 
+	# KEY1 = 47
 	KEY1 = 47
 	KEY2 = 1
 	KEY3 = 1
-	MAXPASS = 5
+	MAXPASS = 50000
 	BORDER = 0.
-	MAXCHISQ = 10.
-	MINDEVIATION = .25
+	MAXCHISQ = 0.01
+	MINDEVIATION = .025
 	NGIVEN = 0
 	LDXGIVEN = NDIM
 	NEXTRA = 0
 	MINEVAL = 0
-	MAXEVAL = 50000
+	MAXEVAL = 1000000
 
-
+	# Integrand = sharpIntegrand
+	Integrand = twopeakIntegrand
 	KEY = 0
-
-	def print_header(name):
-		print('-------------------- %s test -------------------' % name)
-	def print_results(name, results):
-		keys = ['nregions', 'neval', 'fail']
-		text = ["%s %d" % (k, results[k]) for k in keys if k in results]
-		print("%s RESULT:\t" % name.upper() + "\t".join(text))
-		for comp in results['results']:
-			print("%s RESULT:\t" % name.upper() + \
-				"%(integral).8f +- %(error).8f\tp = %(prob).3f\n" % comp)
-
-	from os import environ as env
-	verbose = 2
-	if 'CUBAVERBOSE' in env:
-		verbose = int(env['CUBAVERBOSE'])
+	verbose=0
 
 	print_header('Vegas')
-	print_results('Vegas', pycuba.Vegas(Integrand, NDIM, verbose=2))
+	print_results('Vegas', pycuba.Vegas(Integrand, NDIM, verbose=verbose, epsrel=1e-8))
 
 	print_header('Suave')
-	print_results('Suave', pycuba.Suave(Integrand, NDIM, NNEW, NMIN, FLATNESS, verbose=2 | 4))
+	print_results('Suave', pycuba.Suave(Integrand, NDIM, NNEW, NMIN, FLATNESS, verbose=verbose, epsrel=1e-8))
 
 	print_header('Divonne')
 	print_results('Divonne', pycuba.Divonne(Integrand, NDIM, 
 				mineval=MINEVAL, maxeval=MAXEVAL,
 				key1=KEY1, key2=KEY2, key3=KEY3, maxpass=MAXPASS,
 				border=BORDER, maxchisq=MAXCHISQ, mindeviation=MINDEVIATION,
-				ldxgiven=LDXGIVEN, verbose=2))
+				ldxgiven=LDXGIVEN, verbose=verbose,epsrel=1e-3))
 
 	print_header('Cuhre')
-	print_results('Cuhre', pycuba.Cuhre(Integrand, NDIM, key=KEY, verbose=2 | 4))
+	print_results('Cuhre', pycuba.Cuhre(Integrand, NDIM, key=KEY, verbose=verbose,epsrel=1e-6 ))
 
