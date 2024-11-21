f6e35a6ce6857881bcb28be82e469f4a42813d73
aravindhshankar <aravindhswaminathan@gmail.com>
2024-11-21 19:07:45 +0100

    working version of vectorized CUBA

---
+++ python_files/BLG/testMULTICUBA.py
@@ -0,0 +1,93 @@
+import sys 
+import os
+sys.path.insert(0,'..')
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+from scipy.integrate import simpson, quad
+from functools import partial
+import time 
+from FastRGF.solveRGF import MOMfastrecDOSfull, MOMfastrecNLDOSfull
+from utils.h5_handler import *
+from utils.decorators import cubify
+from utils.models import BLG,Graphene
+import pycuba
+from dask.distributed import Client
+
+savename = 'default_savename'
+path_to_output = '../Outputs/BLG/'
+
+if not os.path.exists(path_to_output):
+    os.makedirs(path_to_output)
+    print("Outputs directory created at ", path_to_output)
+
+if len(sys.argv) > 1:
+    savename = str(sys.argv[1])
+
+
+def test_Ginfkx():
+    # omega = 1 - 1e-2
+    # omega = 2e-4 
+    # omega = 0.000444967
+    # omega = 0.000740532
+    # omega = 2e-2
+    # blg = BLG()	
+    blg = BLG()	
+    ret_H0 = blg.ret_H0
+    ret_Ty = blg.ret_Ty
+    dimH = blg.dimH
+    omega = 3e-4
+    r = 20
+    omegavals = (omega,)
+    dochecks = False
+    delta = 5e-3 * omega # for BLG 
+    # delta = 5e-2 * omega # for graphene 
+    RECURSIONS = 30
+
+    def call_int(kx) : 
+        Gkx = MOMfastrecNLDOSfull(omega,r,kx,ret_H0(kx),ret_Ty(kx),RECURSIONS,delta)
+        return np.array((Gkx[0,0], Gkx[1,1]))
+
+    print('STARTED CUHRE')
+    ##### initialize cubify ######
+    cubify.set_limits(-np.pi,np.pi)
+    Integrand = cubify.VECCubify(call_int)
+    # Integrand = cubify.Cubify(call_int)
+    NDIM = 2
+    KEY = 0
+    MAXEVAL = int(1e5)
+    verbose = 0
+    start_time = time.perf_counter()
+    CUHREdict = pycuba.Cuhre(Integrand, NDIM, ncomp = 2, key=KEY, maxeval=MAXEVAL, verbose=verbose,epsrel=1e-4) 
+    elapsed = time.perf_counter() - start_time
+    print(f'Finished CUHRE in {elapsed} sec(s).')
+    print('CUHRE intval = ', CUHREdict)
+
+
+
+
+
+def test_Gk_single():
+    # kx =  0.0008039 
+    kx =  0.00073387
+    omega = 2e-3
+    RECURSIONS = 30
+    delta = 1e-6
+    DOSkx = MOMfastrecNLDOSfull(omega,r,kx,ret_H0(kx),ret_Ty(kx),RECURSIONS,delta)[0,0]
+    print(DOSkx)
+
+
+if __name__ == '__main__': 
+    test_Ginfkx()
+    # dask_LDOS()
+    # test_LDOS_mp()
+    # test_Gk_single()
+
+
+
+
+
+
+
+
+
--- python_files/utils/decorators.py
+++ python_files/utils/decorators.py
@@ -1,22 +1,34 @@
 from functools import wraps
 
 class cubify:
-	# Class attributes for constans indicating range of integration
-	LOWERLIM = 0.
-	UPPERLIM = 1.
+    # Class attributes for constans indicating range of integration
+    LOWERLIM = 0.
+    UPPERLIM = 1.
 
-	@classmethod
-	def set_limits(cls, a_, b_):
-		cls.LOWERLIM = a_
-		cls.UPPERLIM = b_
+    @classmethod
+    def set_limits(cls, a_, b_):
+        cls.LOWERLIM = a_
+        cls.UPPERLIM = b_
 
-	# Decorator that returns a function of one variable, scales it appropriately for the cuba integration routines
-	@classmethod
-	def Cubify(cls, f):
-		@wraps(f)
-		def wrapper(ndim,xx,ncomp,ff,userdata):
-			x,_ = [xx[i] for i in range(ndim.contents.value)]
-			ff[0] = (cls.UPPERLIM-cls.LOWERLIM) * f(cls.LOWERLIM + (cls.UPPERLIM-cls.LOWERLIM)*x)
-			return 0
-		return wrapper 
+    # Decorator that returns a function of one variable, scales it appropriately for the cuba integration routines
+    @classmethod
+    def Cubify(cls, f):
+        @wraps(f)
+        def wrapper(ndim,xx,ncomp,ff,userdata):
+            x,_ = [xx[i] for i in range(ndim.contents.value)]
+            ff[0] = (cls.UPPERLIM-cls.LOWERLIM) * f(cls.LOWERLIM + (cls.UPPERLIM-cls.LOWERLIM)*x)
+            return 0
+        return wrapper 
 
+    # When the function returns a vector 
+    @classmethod
+    def VECCubify(cls, f):
+        @wraps(f)
+        def wrapper(ndim,xx,ncomp,ff,userdata):
+            x,_ = [xx[i] for i in range(ndim.contents.value)]
+            # ff = (cls.UPPERLIM-cls.LOWERLIM) * f(cls.LOWERLIM + (cls.UPPERLIM-cls.LOWERLIM)*x)
+            ffO = (cls.UPPERLIM-cls.LOWERLIM) * f(cls.LOWERLIM + (cls.UPPERLIM-cls.LOWERLIM)*x)
+            for j in range(ncomp.contents.value):
+                ff[j] = ffO[j]
+            return 0
+        return wrapper 
