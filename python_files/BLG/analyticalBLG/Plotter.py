361cc9fbfbbf0ab78659d3752e6edd02a8efc82a
aravindhshankar <aravindhswaminathan@gmail.com>
2024-12-06 13:44:22 +0100

    Update Plotter.py

--- python_files/BLG/analyticalBLG/Plotter.py
+++ python_files/BLG/analyticalBLG/Plotter.py
@@ -7,34 +7,27 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from utils.h5_handler import *
 from scipy.special import j0
-path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/BLGnldosTurnoffg4delp'
-path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/BLGnldosfigsTurnoffg4delp'
+path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/anaBLG/'
+path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/anaBLG'
 # path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/BLGnldos'
 # path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/BLGnldosfigs'
-if not os.path.exists(path_to_dump): 
+if not os.path.exists(path_to_dump):
 	raise exception('path to dump not found')
 	exit(1)
 
 
-def analyticNLDOS_graphene(omega,r,vF):
-    ''' 
-    Later put this in the class definition once factors of pi are resolved 
-    '''
-    pref = np.pi * omega / (vF**2)
-    return pref * j0(np.pi*r*omega/vF) / np.pi #Spurious factors of pi as usual
 
-
-NUMGS = 4 #number of dictionary entries in the load file
+NUMGS = 1 #number of dictionary entries in the load file
 
 figlist = [plt.figure() for i in range(NUMGS)]
 axlist = [figlist[i].subplots(2) for i in range(NUMGS)]
 
-# jobarray = np.arange(0,20,4,dtype=int)
+jobarray = np.arange(0,13,1,dtype=int)
 # jobarray = [0,5,12,15,20]
-jobarray = [0,1,2]
+# jobarray = [0,1,2]
 for i, job_idx in enumerate(jobarray):
     col = 'C' + str(i)
-    filename = f'results_r_{job_idx}.h5'
+    filename = f'results_g3_{job_idx}.h5'
     try:
         load_dict = h52dict(os.path.join(path_to_dump,filename))
     except FileNotFoundError:
@@ -44,20 +37,19 @@ for i, job_idx in enumerate(jobarray):
 
     print('keys = ', load_dict.keys()) if __debug__ else 0
     omegavals = load_dict['omegavals']
-    NLDOS = np.array(load_dict['NLDOS'])
-    rval = load_dict['r']
-    # analytic = np.array([analyticNLDOS_graphene(omegaval,rval,vF) for omegaval in omegavals])
+    LDOS = np.array(load_dict['LDOS'])
+    g3val = load_dict['gamma3']
     print(load_dict['INFO']) if __debug__ else 0
     for j, ax in enumerate(axlist):
-        ax[0].plot(omegavals, NLDOS.T[j], '.-', c=col, label = f'r = {rval}')
+        ax[0].plot(omegavals, LDOS, '.-', c=col, label = f'g3 = {g3val}')
         # ax[0].plot(omegavals, analytic, '--', c=col)
-        ax[1].loglog(omegavals[omegavals<1],NLDOS.T[j][omegavals<1],c=col, label = f'r = {rval}')
+        ax[1].loglog(omegavals[omegavals<1],LDOS[omegavals<1],c=col, label = f'g3 = {g3val}')
 
         ax[0].set_xlabel('omega')
-        ax[0].set_title(f'BLG NLDOS site [{j},{j}] ')
+        ax[0].set_title(f'BLG scan gamma3 LDOS site Analytical two band ')
         ax[0].legend()
-        ax[0].set_ylabel(r'G(r,$\omega$)')
-        ax[1].set_ylabel(r'G(r,$\omega$)')
+        ax[0].set_ylabel(r'G($\omega$)')
+        ax[1].set_ylabel(r'G($\omega$)')
         ax[1].set_xlabel('omega')
         ax[1].legend()
 
