2441d7f47628b2fd8c30e2faea7797028e1ff9ab
Shankar <55738744+aravindhshankar@users.noreply.github.com>
2024-12-08 14:04:42 +0100

    changed Plotter

--- python_files/BLG/BLGparNLDOS/PlotterNLDOS.py
+++ python_files/BLG/BLGparNLDOS/PlotterNLDOS.py
@@ -7,14 +7,18 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from utils.h5_handler import *
 from scipy.special import j0
-path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/BLGnldosTurnoffg4delp'
-path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/BLGnldosfigsTurnoffg4delp'
-# path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/BLGnldos'
-# path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/BLGnldosfigs'
+# path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/BLGnldosTurnoffg4delp'
+# path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/BLGnldosfigsTurnoffg4delp'
+path_to_dump = '../../Output/NEWBLGnldos/'
+path_to_fig = '../../Figures/NEWBLGnldos/'
 if not os.path.exists(path_to_dump): 
 	raise exception('path to dump not found')
 	exit(1)
 
+if not os.path.exists(path_to_fig):
+    print('Path to fig not found, creating ......')
+    os.makedirs(path_to_fig)
+    print('Created fig directory at ', path_to_fig)
 
 def analyticNLDOS_graphene(omega,r,vF):
     ''' 
@@ -31,7 +35,7 @@ axlist = [figlist[i].subplots(2) for i in range(NUMGS)]
 
 # jobarray = np.arange(0,20,4,dtype=int)
 # jobarray = [0,5,12,15,20]
-jobarray = [0,1,2]
+jobarray = [0,]
 for i, job_idx in enumerate(jobarray):
     col = 'C' + str(i)
     filename = f'results_r_{job_idx}.h5'
@@ -61,8 +65,8 @@ for i, job_idx in enumerate(jobarray):
         ax[1].set_xlabel('omega')
         ax[1].legend()
 
-# for j in range(NUMGS):
-    # savefigname = f'BLGNLDOS_{j,j}.pdf'
-    # figlist[j].savefig(os.path.join(path_to_fig, savefigname))
+for j in range(NUMGS):
+    savefigname = f'BLGNLDOS_{j,j}.pdf'
+    figlist[j].savefig(os.path.join(path_to_fig, savefigname))
 
 plt.show()
