6f785a3ad7a3cc2d9e06f0701c28eea2c5ac7c89
aravindhshankar <aravindhswaminathan@gmail.com>
2024-12-17 14:55:54 +0100

    for BLG, delta=5e-3 * omega is sufficient

--- python_files/BLG/scandeltavH/Plotter.py
+++ python_files/BLG/scandeltavH/Plotter.py
@@ -1,3 +1,4 @@
+###### Learning : for BLG, delta = 5e-3 * omega should be sufficient
 import sys
 import os
 sys.path.insert(0,'..')
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from utils.h5_handler import *
 from scipy.special import j0
+from scipy.integrate import simpson
 # path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/scang3/NewBLGTdag/'
 # path_to_fig = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Figures/scang3/NewBLGTdag/'
 path_to_dump = '/Users/aravindhswaminathan/Documents/GitHub/RKKYTightBinding/python_files/Output/scandeltaVH/'
@@ -28,6 +30,7 @@ axlist = [figlist[i].subplots(2) for i in range(NUMGS)]
 jobarray = np.arange(0,10,1,dtype=int)
 # jobarray = [0,5,12,15,20]
 # jobarray = [5,]
+areas = np.array([np.zeros_like(jobarray,dtype=np.float64) for i in range(NUMGS)]).T
 for i, job_idx in enumerate(jobarray):
     col = 'C' + str(i)
     filename = f'results_delta_{job_idx}.h5'
@@ -42,8 +45,14 @@ for i, job_idx in enumerate(jobarray):
     omegavals = load_dict['omegavals']
     LDOS = np.array(load_dict['LDOS'])
     deltaval = load_dict['delta']
+    ####### Total spectral weight ######
+
     print(load_dict['INFO']) if __debug__ else 0
     for j, ax in enumerate(axlist):
+        np.testing.assert_equal(len(LDOS.T[j]), len(omegavals))
+        areas[i][j] = simpson(y = LDOS.T[j], x = omegavals)
+        print(simpson(y = LDOS.T[j], x = omegavals))
+        print(areas[i][j])
         ax[0].plot(omegavals, LDOS.T[j], '.-', c=col, label = f'delta = {deltaval}')
         # ax[0].plot(omegavals, analytic, '--', c=col)
         ax[1].loglog(omegavals[omegavals<1],LDOS.T[j][omegavals<1],c=col, label = f'delta = {deltaval}')
@@ -59,5 +68,13 @@ for i, job_idx in enumerate(jobarray):
 for j in range(NUMGS):
     savefigname = f'BLGNLDOS_{j,j}.pdf'
     figlist[j].savefig(os.path.join(path_to_fig, savefigname))
+
+
+print(areas)
+print(f'Areas = ', areas[:,0])
+print(f'combined areas = ', np.sum(areas,axis=1))
+
+
+
 
 plt.show()
