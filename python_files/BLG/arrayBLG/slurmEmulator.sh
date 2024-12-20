efc80c503ceae97898ab148e895d0882b02815a1
aravindhshankar <aravindhswaminathan@gmail.com>
2024-12-20 12:35:42 +0100

    Update slurmEmulator.sh

--- python_files/BLG/arrayBLG/slurmEmulator.sh
+++ python_files/BLG/arrayBLG/slurmEmulator.sh
@@ -6,9 +6,10 @@ source /data1/shankar/RKKYTightBinding/.BLGvenv/bin/activate  #path to venv inst
 LOGS=logs
 mkdir -p $LOGS
 #
-for SLURM_ARRAY_TASK_ID in {10}
+for SLURM_ARRAY_TASK_ID in {10,}
 do 
 	echo "Starting task $SLURM_ARRAY_TASK_ID"
+	export SLURM_ARRAY_TASK_ID
 	python3.10 grid_calculation.py > $LOGS/L_$SLURM_ARRAY_TASK_ID.out 2> $LOGS/L_$SLURM_ARRAY_TASK_ID.err 
 	echo "Task ID $SLURM_ARRAY_TASK_ID completed"
 done
