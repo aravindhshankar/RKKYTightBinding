d0ef156e88c15f5ef7a382f2b8b989c35c150e81
Shankar Aravindh Swaminathan <ashankar@ictp.it>
2025-01-24 11:42:00 +0100

    Added daskification of intAnalytical.py

--- python_files/BLG/analyticalBLG/intAnalytical.py
+++ python_files/BLG/analyticalBLG/intAnalytical.py
@@ -1,3 +1,4 @@
+#NOTE currently supported only for LDOS, i.e only pass 0 as command line argument
 import sys 
 import os
 sys.path.insert(0,'..')
@@ -24,6 +25,10 @@ if not os.path.exists(path_to_output):
 print = partial(print, flush=True) #To see output at each step in alice
 
 def helper_mp(omega,r):
+    ''' 
+    This should be Dasked - delayed(helper_mp, omegavals)
+    Currently there is only support for r = 0!!!!
+    '''
     # delta = 5e-3 * omega # for BLG 
     delta = 5e-2 * omega # for turning off gamma 4 this is better 
     default_blg = BLG()
@@ -57,46 +62,45 @@ def helper_mp(omega,r):
     return intarr
 
 
-def process_g3(g3_index):
-    g3_values = np.array([0.01,0.1,0.2,0.3,0.36,0.38,0.4,0.5,0.6,0.7,0.8,1.0,3.0]) # list of gamma3 values to scan , 5th 0 based index is the expt one
-    if g3_index > len(g3_values) - 1 :
-        raise(Exception('g3_index OUT OF BOUNDS!!!!!! aborting......'))
-        exit(1)
-    g3 = g3_values[g3_index]
-    # omegavals = [0.0003,0.003,0.03,0.3]
+def process_r(r_index):
+    r_values = np.arange(21,dtype=int)  # r = 0 is the LDOS, goes until r = 20
+    r = r_values[r_index]
+    # omegavals = np.array([0.0003,0.003,0.03,0.3])
     eps = 1e-5
-    omegavals = np.sort(np.concatenate((np.logspace(np.log10(1e-6),np.log10(1e-2),300),np.linspace(1e-2+eps,1.2,60))))
+    omegavals = np.sort(np.concatenate((np.logspace(np.log10(1e-6),np.log10(1e-2),300),np.linspace(1e-2+eps,5e-1,50))))
+    omegavals = np.concatenate((-1.*omegavals[::-1], omegavals))
 
-    PROCESSES = int(os.environ.get('SLURM_CPUS_PER_TASK','2'))
+    PROCESSES = int(os.environ.get('SLURM_CPUS_PER_TASK','6'))
     start_time = time.perf_counter()
-    # Initialize Dask cluster
-    # scheduler_port = 8786 + r_index
-    # local_dir = f"/tmp/dask-task-{r_index}-{os.getpid()}"
-    # cluster = LocalCluster(n_workers=PROCESSES, processes=True, scheduler_port=scheduler_port, local_directory=local_dir) 
     cluster = LocalCluster(n_workers=PROCESSES, processes=True) 
     client = Client(cluster)
 
     # Create Dask tasks
-    tasks = [delayed(helper_mp)(omega, g3) for omega in omegavals] #returns the NLDOS as function of omega for a given r
+    tasks = [delayed(helper_mp)(omega, r) for omega in omegavals] #returns the NLDOS as function of omega for a given r
     results = client.compute(tasks)
     results = client.gather(results)
 
     client.close()
     elapsed = time.perf_counter() - start_time
-    print(f'Dask process with {PROCESSES} processes for gamma3  = {g3} finished in {elapsed} sec(s).')
+    print(f'Dask process with {PROCESSES} processes for r = {r} finished in {elapsed} sec(s).')
     # Return a dictionary that needs to be saved 
     savedict = {'omegavals' : omegavals,
-                'gamma3' : g3,
-                'LDOS' : results,
-                'INFO' : 'two band model based on mccann-koshino with only h0 and hw , delta = 5e-2 * omega, deltprime=gamma4=0'
+                'r' : r,
+                'NLDOS' : results,
+                'INFO' : 'A site of the 2x2 reduced BLG model , delta = 5e-2 * omega '
                 }
     return savedict
+
 
 if __name__ == "__main__":
     # Get the Slurm task ID
     task_id = int(sys.argv[1])  # Passed by Slurm as an argument
-    save_dict = process_g3(task_id)
-    savename = f"results_g3_{task_id}"
+    if task_id != 0:
+        raise(Exception('CURRENTLY ONLY LDOS SUPPORTED!!!! COMMAND LINE ARGS OTHER THAN 0 ARE NOT ACCEPTED'))
+        exit(1)
+    
+    save_dict = process_r(task_id)
+    savename = f"results_r_{task_id}"
     savefileoutput = savename + '.h5'
     dict2h5(save_dict,os.path.join(path_to_output,savefileoutput), verbose=True)
 
