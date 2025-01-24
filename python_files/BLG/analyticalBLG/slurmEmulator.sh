87af2973640e2e1d36c7a5cc652d9ca568a109e9
Shankar Aravindh Swaminathan <ashankar@ictp.it>
2025-01-24 12:02:56 +0100

    Updated slurmEmulator with environment from ictp

--- python_files/BLG/analyticalBLG/slurmEmulator.sh
+++ python_files/BLG/analyticalBLG/slurmEmulator.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -xe
 export SLURM_CPUS_PER_TASK=6
-source /data1/shankar/RKKYTightBinding/.BLGvenv/bin/activate  #path to venv installed using requirements.txt
+#source /data1/shankar/RKKYTightBinding/.BLGvenv/bin/activate  #path to venv installed using requirements.txt
+source /scratch/ashankar/RKKYTightBinding/.BLGvenv/bin/activate  #path to venv installed using requirements.txt
 #
-LOGS=logs/turnoffg4
+LOGS=logs/Correct2BandLDOS
 mkdir -p $LOGS
 #
-for SLURM_ARRAY_TASK_ID in {0..12}
+#for SLURM_ARRAY_TASK_ID in {0..12}
+for SLURM_ARRAY_TASK_ID in {0,}
 do 
 	echo "Starting task $SLURM_ARRAY_TASK_ID"
-	python3.10 intAnalytical.py $SLURM_ARRAY_TASK_ID > $LOGS/g3_$SLURM_ARRAY_TASK_ID.out 2> $LOGS/g3_$SLURM_ARRAY_TASK_ID.err 
+	python3.10 intAnalytical.py $SLURM_ARRAY_TASK_ID > $LOGS/r_$SLURM_ARRAY_TASK_ID.out 2> $LOGS/r_$SLURM_ARRAY_TASK_ID.err 
 	echo "Task ID $SLURM_ARRAY_TASK_ID completed"
 done
 #
