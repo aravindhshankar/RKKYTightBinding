Changed all minus signs to fit with correct right hopping matrices and fourier transforms

python_files/BLG/v2symmdelta2arrayBLG/grid_config.py
@@ -47,7 +47,7 @@ def helper_mp(r, omega):
     @cubify.VECCubify
     def call_int(kx) : 
         # Gkx = MOMfastrecNLDOSfull(omega,r,kx,ret_H0(kx),ret_Ty(kx).conj().T,RECURSIONS,delta) #adding the right hopping matrix as needed for fast recursion in v2
-        Gkx = MOMfastrecNLDOSfull(omega,r,kx,ret_H0(kx),ret_Ty(kx).T,RECURSIONS,delta) #adding the right hopping matrix as needed for fast recursion in v2
+        Gkx = MOMfastrecNLDOSfull(omega,r,kx,ret_H0(kx).T,ret_Ty(kx).T,RECURSIONS,delta) #adding the right hopping matrix as needed for fast recursion in v2
         return np.array((Gkx[0,0], Gkx[1,1], Gkx[2,2], Gkx[3,3], Gkx[0,4], Gkx[1,5], Gkx[2,6], Gkx[3,7]))
 
     ################### starting integration #################

python_files/FastRGF/solveRGF.py
@@ -164,7 +164,8 @@ def MOMfastrecNLDOSfull(omega,r,kx,H0,Ty,RECURSIONS=20,delta=0.001,dochecks=Fals
 			warnings.warn(f"FOUND overflow in itrmdt for omega = {omega}")
 			return np.zeros_like(H0,dtype=np.double)
 		G = custinv(itrmdt) 
-		NLDOS = (0.5/np.pi) * (np.real(G)* np.sin(kx*r) - np.imag(G)*np.cos(kx*r)) #TODO:notice the relative factor of 2 in comparison to definition of LDOS
+		NLDOS = (-0.5/np.pi) * (np.real(G)* np.sin(kx*r) + np.imag(G)*np.cos(kx*r)) #This is with the correct convention of fourier transform
+		# NLDOS = (0.5/np.pi) * (np.real(G)* np.sin(kx*r) - np.imag(G)*np.cos(kx*r)) #TODO:notice the relative factor of 2 in comparison to definition of LDOS
 		if dochecks == True and (np.isnan(NLDOS).any() or np.isinf(NLDOS).any()):
 			warnings.warn(f"FOUND overflow in NLDOS for omega = {omega}")
 			return np.zeros_like(H0,dtype=np.double)